283ae5c1dc65a059ab943a56946cc7b53e57e681
NusratJahanBristy <hellonusratjahanbristy@gmail.com>
2022-09-15 00:51:56 +0600

    dynamic count fixed

--- script.js
+++ script.js
@@ -104,10 +104,11 @@ const start = () => {
   if (startTime) return;
 
   let count = 3;
+  
   countdownOverlay.style.display = "flex";
-
+  
   const startCountdown = setInterval(() => {
-    countdownOverlay.innerHTML = '<h1>${count}</h1>';
+    countdownOverlay.innerHTML = `<h1>${count}</h1>`;
 
     // finished timer
     if (count == 0) {
